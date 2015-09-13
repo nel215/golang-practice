09cc8c8bf8f22f6ce90584c5a81740b4d8e71b3d
nel215 <otomo.yuhei@gmail.com>
2015-09-13 21:35:46 +0900

    added an actual server test

--- main_test.go
+++ main_test.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"bufio"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func TestHandleHi_TestServer(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handleHi))
+	defer ts.Close()
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if g, w := res.Header.Get("Content-Type"), "text/html; charset=utf-8"; g != w {
+		t.Errorf("Content-Type = %q; want %q", g, w)
+	}
+	slurp, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("Got: %s", slurp)
+}
 
 func TestHandleHi_Recorder(t *testing.T) {
 	rw := httptest.NewRecorder()
